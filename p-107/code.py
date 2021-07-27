28fde4c3030c1bdd3e3f2b850ef13d824899882a
AryanRagupathy <67152544+AryanRagupathy@users.noreply.github.com>
2021-07-27 16:14:59 +0530

    Add files via upload

---
+++ p-107/code.py
@@ -0,0 +1,30 @@
+import csv
+import pandas as pd
+import plotly.express as px
+
+px.defaults.template = "plotly_dark"
+
+df = pd.read_csv("data.csv")
+
+grouped_mean = df.groupby(["Student ID", "Level"], as_index = False)["Attempt"].mean()
+
+
+chart = px.scatter(grouped_mean,
+                x = "Student ID",
+                y = "Level",
+                size = "Attempt",
+                color = "Attempt",
+                title = "Performance of Students in each Level",
+                )
+
+chart.update_layout(
+                font_family="Papyrus, fantasy",
+                title_x = 0.5,
+                font_size = 20,
+                title_font_size = 30,
+               
+                )
+
+
+
+chart.show()
